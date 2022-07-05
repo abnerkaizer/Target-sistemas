f8a8f8c65d723b5e000d8cfb0b5149efd77c78a0
Abner <abner@abner-pc.localdomain>
2022-07-05 11:14:43 -0300

    fmt

--- n3/src/main.rs
+++ n3/src/main.rs
@@ -1,21 +1,20 @@
-use std::fs;
 use serde_json::from_str;
-
+use std::fs;
 
 fn main() {
     let data = fs::read_to_string("dados.json").expect("Incapaz de ler o arquivo.");
-    let json:serde_json::Value = from_str(&data).expect("JSON não está bem formatado.");
+    let json: serde_json::Value = from_str(&data).expect("JSON não está bem formatado.");
     let json = json.as_array().unwrap();
-    let mut maior:f64 = 0.0;
-    let mut menor:f64 = f64::MAX;
-    let mut media:f64 = 0.0;
-    for dia in  json.iter(){
-        let valor:f64  = dia["valor"].as_f64().unwrap();
-        media+=valor/30.0;
-        if valor <menor {
+    let mut maior: f64 = 0.0;
+    let mut menor: f64 = f64::MAX;
+    let mut media: f64 = 0.0;
+    for dia in json.iter() {
+        let valor: f64 = dia["valor"].as_f64().unwrap();
+        media += valor / 30.0;
+        if valor < menor {
             menor = valor;
         }
-        if valor>maior {
+        if valor > maior {
             maior = valor;
         }
     }
@@ -23,10 +22,13 @@ fn main() {
     println!("O maior faturamento foi de {}.", maior);
     let mut soma = 0;
     for dia in json.iter() {
-        let valor:f64  = dia["valor"].as_f64().unwrap();
-        if valor>media {
-            soma+=1;
+        let valor: f64 = dia["valor"].as_f64().unwrap();
+        if valor > media {
+            soma += 1;
         }
     }
-    println!("Foram {} dias que superaram a media ({:.4}) de faturamento do mês.", soma,media);
+    println!(
+        "Foram {} dias que superaram a media ({:.4}) de faturamento do mês.",
+        soma, media
+    );
 }
--- n4/src/main.rs
+++ n4/src/main.rs
@@ -4,10 +4,10 @@ fn main() {
     let mg = 29_229.88;
     let es = 27_165.48;
     let outros = 19_849.53;
-    let total = sp+rj+mg+es+outros;
-    println!("SP tem {:.2}% do total.", (sp/total)*100.0);
-    println!("RJ tem {:.2}% do total.", (rj/total)*100.0);
-    println!("MG tem {:.2}% do total.", (mg/total)*100.0);
-    println!("ES tem {:.2}% do total.", (mg/total)*100.0);
-    println!("Outros tem {:.2}% do total.", (outros/total)*100.0);
+    let total = sp + rj + mg + es + outros;
+    println!("SP tem {:.2}% do total.", (sp / total) * 100.0);
+    println!("RJ tem {:.2}% do total.", (rj / total) * 100.0);
+    println!("MG tem {:.2}% do total.", (mg / total) * 100.0);
+    println!("ES tem {:.2}% do total.", (mg / total) * 100.0);
+    println!("Outros tem {:.2}% do total.", (outros / total) * 100.0);
 }
